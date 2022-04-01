9d0f9e4bd26a2b0a9dd1540accffc438598ad625
Mohammed Musharaf Z <musharaff87@gmail.com>
2022-04-01 12:57:47 +0530

    commit 1

--- module_1.py
+++ module_1.py
@@ -1,9 +1,9 @@
 import cv2
 w, h = 640, 480
-cap = cv2.VideoCapture(0)
-cap.set(3, w)
-cap.set(4, h)
+c = cv2.VideoCapture(0)
+c.set(3, w)
+c.set(4, h)
 while True:
-    success, img = cap.read()
-    cv2.imshow("Image", img)
+    success, obj = c.read()
+    cv2.imshow("Image", obj)
     cv2.waitKey(1)
