628e442458f14f350741b9d1c4079e2cdaba82f0
Mohammed Musharaf Z <musharaff87@gmail.com>
2022-04-01 14:57:24 +0530

    commit 2

--- module_1.py
+++ module_1.py
@@ -4,6 +4,15 @@ c = cv2.VideoCapture(0)
 c.set(3, w)
 c.set(4, h)
 while True:
-    success, obj = c.read()
-    cv2.imshow("Image", obj)
+
+    success, img = c.read()
+    img = findHands(img)
+    lmList = findPosition(img)
+
+    if len(lmList) != 0:
+        x1, y1 = lmList[8][1:]
+        x2, y2 = lmList[12][1:]
+        print(x1, y1, x2, y2)
+
+    cv2.imshow("Frame", obj)
     cv2.waitKey(1)
